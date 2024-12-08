e2296030eb7e346b25a62889f2773bc385488deb
Mitchell Wintrow <90mitchwintrow@gmail.com>
2024-12-08 00:58:27 -0700

    Add initial payment server

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# AspireWithAlinaPaymentServer
+# Aspire With Alina payment server
+
+## Contact info
+
+**Developed by:** Mitchell Wintrow
+
+**Email**: owner@winapps.io, mitchellwintrow@gmail.com
+
+## Technologies
+
+![NodeJS](https://img.shields.io/badge/Node.js-6DA55F?logo=node.js&logoColor=white)
+![Stripe](https://img.shields.io/badge/Stripe-5851DD?logo=stripe&logoColor=fff)
+![PayPal](https://img.shields.io/badge/PayPal-003087?logo=paypal&logoColor=fff)
+
+## License
+
+This project is licensed under the **Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License**.
+
+- You can view and share this code for **non-commercial purposes** as long as proper credit is given.
+- **Forking, modifications, or derivative works are not allowed.**
+
+For the full license text, visit [Creative Commons License](https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode).
+
+---
+
+This product is developed and owned by [WinApps Solutions LLC ©2024](https://winapps.io)
+
+![WinApps Logo](./WinAppsLogo.svg)
---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const https = require("https");
+const fs = require("fs");
+const bodyParser = require("body-parser");
+const Stripe = require("stripe");
+
+const options = {
+  cert: fs.readFileSync("/etc/letsencrypt/live/aspirewithalina.com/fullchain.pem"),
+  key: fs.readFileSync("/etc/letsencrypt/live/aspirewithalina.com/privkey.pem"),
+};
+const app = express();
+const PORT = 7777;
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2022-11-15",
+});
+const lessonPackages = [
+  { id: "1_lesson", name: "1 Lesson", price: 40 }, // $40 per lesson
+  { id: "3_lessons", name: "3 Lessons", price: 114 }, // $38 per lesson
+  { id: "6_lessons", name: "6 Lessons", price: 210 }, // $35 per lesson
+  { id: "12_lessons", name: "12 Lessons", price: 360 }, // $30 per lesson
+];
+
+app.use(bodyParser.json());
+
+app.post("/create-payment-intent", async (req, res) => {
+  const { packageId } = req.body;
+
+  const selectedPackage = lessonPackages.find((pkg) => pkg.id === packageId);
+  if (!selectedPackage) {
+    res.status(400).send("Invalid package ID");
+    return;
+  }
+
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: selectedPackage.price * 100,
+      currency: "usd",
+      automatic_payment_methods: { enabled: true },
+    });
+
+    res.json({ clientSecret: paymentIntent.client_secret });
+  } catch (error) {
+    console.error(`Payment intent creation failed: ${error.message}`);
+    res.status(500).send("An error occurred while creating the payment intent");
+  }
+});
+
+https.createServer(options, app).listen(PORT, () => {
+  console.log(`Server is running securely on https://aspirewithalina.com:${PORT}`);
+});
