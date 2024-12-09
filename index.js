677eb4cc751b90a22ebd026fdc3e0b25e166aa99
Mitchell Wintrow <90mitchwintrow@gmail.com>
2024-12-08 23:20:18 -0700

    Fix stripe secret key usage

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const options = {
 };
 const app = express();
 const PORT = 7777;
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: "2022-11-15",
 });
 const lessonPackages = [
